b06abbb5c528ef4a1378b06e3f1426d8bfab8a83
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-10-29 13:47:53 +0300

    styling of popular products section and the cards

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,7 +1,28 @@
-
+import PopularProductCard from "../components/PopularProductCard"
+import { products } from "../constants"
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section
+      id="products"
+      className="max-container max-sm:mt-12"
+    >
+      <div className="felx flex-col justify-start gap-5">
+
+        <h2 className="text-4xl font-palanquin font-bold">Our
+          <span className="text-coral-red"> Popular </span>
+          Products
+        </h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray"> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia officia in, ratione ea animi modi veniam et maiores cum libero eius est dicta minima unde placeat voluptate consequuntur ex repellendus?</p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.id} {...product}/>
+        ))}
+
+      </div>
+
+    </section>
   )
 }
 
